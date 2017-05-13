04107a65265a9e3c57115cc9c486e94eef03cfba
Vladimir <vladvladow@yandex.ru>
2017-05-13 18:13:04 +0500

    Load script with async=false

--- inlining-js/check.js
+++ inlining-js/check.js
@@ -5,10 +5,14 @@ javascript:(function inline(){
     };
 
     var md5s=document.createElement('script');
+    md5s.type = 'text/javascript';
+    md5s.async = false;
     md5s.src='https://ros-tel.github.io/stopforumspam/inlining-js/md5.js';
     document.body.appendChild(md5s);
 
     var check=document.createElement('script');
+    check.type = 'text/javascript';
+    check.async = false;
     check.src='https://ros-tel.github.io/stopforumspam/inlining-js/phpBB_check.js';
     document.body.appendChild(check);
 })();
--- inlining-js/insert.js
+++ inlining-js/insert.js
@@ -5,10 +5,14 @@ javascript:(function inline(){
     };
 
     var md5s=document.createElement('script');
+    md5s.type = 'text/javascript';
+    md5s.async = false;
     md5s.src='https://ros-tel.github.io/stopforumspam/inlining-js/md5.js';
     document.body.appendChild(md5s);
 
     var insert=document.createElement('script');
+    insert.type = 'text/javascript';
+    insert.async = false;
     insert.src='https://ros-tel.github.io/stopforumspam/inlining-js/phpBB_insert.js';
     document.body.appendChild(insert);
 })();
